03fcb92434f671443f65c03fd605ead4c4f3c294
roy <loitp@galaxyjoy.vn>
2025-09-12 21:13:34 +0700

    Update app version information
    This commit updates the `versionCode` to `20250912` and `versionName` to `2025.09.12` in the `app/build.gradle.kts` file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         minSdk = 23
         //noinspection EditedTargetSdkVersion
         targetSdk = 35
-        versionCode = 20250816
-        versionName = "2025.08.16"
+        versionCode = 20250912
+        versionName = "2025.09.12"
 
         vectorDrawables.useSupportLibrary = true
         multiDexEnabled = true
