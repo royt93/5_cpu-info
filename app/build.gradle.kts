712e7c27810a3466abd32be1ba378fd82fdcd210
roy <loitp@galaxyjoy.vn>
2026-05-16 16:11:39 +0700

    Update version code
    This commit updates the application's configuration in `app/build.gradle.kts`:
    
    - **Update Versioning**: The `versionCode` has been updated from `20250324` to `20260324`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         minSdk = 24
         //noinspection EditedTargetSdkVersion
         targetSdk = 36
-        versionCode = 20260324
-        versionName = "2026.03.24"
+        versionCode = 20260516
+        versionName = "2026.05.16"
 
         vectorDrawables.useSupportLibrary = true
         multiDexEnabled = true
